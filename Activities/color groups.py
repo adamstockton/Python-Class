4caf9ab64c9c648130ce9630537f337707300e7c
Adam Stockton <adamstockton22@gmail.com>
2020-07-07 01:18:29 -0400

    Color groups activity template

---
+++ Activities/color groups.py
@@ -0,0 +1,19 @@
+# Define Grid
+grid = [
+    ["G", "G", "B", "R"], 
+    ["G", "B", "R", "B"], 
+    ["R", "B", "B", "B"]
+]
+
+# Capture Results
+highest = {
+    "color": "",
+    "count": 0
+}
+
+# Insert Code Here
+# Find the most continuous color within the grid and capture the cell count
+
+
+# Output Results
+print('The highest color is {} with a count of {}'.format(highest['color'], highest['count']))
