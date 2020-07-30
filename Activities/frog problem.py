33f4e8ba6d3328a70268a27e8ece18f2d2332ab2
Adam Stockton <adamstockton22@gmail.com>
2020-07-30 00:33:01 -0400

    Fixed bug with range

--- Activities/frog problem.py
+++ Activities/frog problem.py
@@ -6,9 +6,9 @@ results = []
 
 for i in range(cap):
     total = 1
-    spot = random.randint(0, river_width + 1)
-    while(spot < 10):
-        spot += random.randint(1, (river_width - spot))
+    spot = random.randint(0, river_width - 1)
+    while(spot < 9):
+        spot += random.randint(1, ((river_width - 1) - spot))
         total += 1
     results.append(total)
 
